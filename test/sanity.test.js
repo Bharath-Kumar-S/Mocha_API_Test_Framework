55dbcc8dd3cd20b3925501741a44420313d62b58
Bharath Kumar S <bharathkumarkarthick@gmail.com>
2020-10-01 20:38:19 +0530

    medium fix

--- test/sanity.test.js
+++ test/sanity.test.js
@@ -7,18 +7,21 @@ describe('Sample api tests', () => {
     it('GET request', async () => {
         let res = await fetch('https://api.github.com/users/github', { method: 'GET' });
         let response = await res.json();
-        // console.log(response);
         expect(response).to.haveOwnProperty('login')
         expect(response.login).to.contain('github');
     })
 
     it('POST request', async () => {
-        const FormData = require('form-data');
-        const form = new FormData();
-        form.append('a', 1);
-        let res = await fetch('https://httpbin.org/post', { method: 'POST', body: form })
+        let res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST', body: {
+                "userId": 1,
+                "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+            }
+        })
         let response = await res.json();
-        // console.log(response);
+        console.log(response);
+        expect(response.id).to.equal(101)
     })
 
 })
